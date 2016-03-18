34f4dbab6130b416f468b7475cfe292b8ccc042c
dennisrcs <denisrcs@gmail.com>
2016-03-18 13:25:59 -0500

    first commit. neuron class created

---
+++ MLP/src/model/Neuron.java
@@ -0,0 +1,17 @@
+package model;
+
+// represents the neuron of the neural network
+public class Neuron
+{
+	// members
+	private int value;
+
+	// getters and setters
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
